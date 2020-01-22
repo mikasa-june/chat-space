a6118729502448d8f3e39066de035bcc1e253906
mikasa-june <59766405+mikasa-june@users.noreply.github.com>
2020-01-22 14:16:22 +0900

    doneとfalseの作成と日本時間への変更

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,45 @@
 $(function(){
+  function buildHTML(message){
+    console.log(message)
+    if (message.image) {
+      var html =
+      `<div class="message">
+        <div class="user-info">
+          <div class="user-name">
+            ${message.user_name}
+          </div>
+          <div class="date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message-text">
+          <p class=".lower-message__content">
+            ${message.content}
+          </p>
+          <img src=${message.image} >
+        </div>
+      </div>`
+      return html;
+    } else {
+      var html =
+      `<div class="message">
+      <div class="user-info">
+        <div class="user-name">
+          ${message.user_name}
+        </div>
+        <div class="date">
+          ${message.created_at}
+        </div>
+      </div>
+      <div class="message-text">
+        <p class=".lower-message__content">
+          ${message.content}
+        </p>
+      </div>
+    </div>`
+      return html;
+    };
+  }
   $("#new_message").on("submit", function(e){
     e.preventDefault()
     var formData = new FormData(this);
@@ -11,5 +52,15 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      $('form')[0].reset();
+      $('.send-btn').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
   })
 });
