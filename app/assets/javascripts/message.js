1ad8c668ba33c1106c7569ffa7318b9eaae8ec82
mikasa-june <59766405+mikasa-june@users.noreply.github.com>
2020-01-23 14:59:05 +0900

    自動更新機能の修正#2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,5 @@
 $(function(){
   last_message_id = $('.message:last').data("message-id");
-  console.log(last_message_id);
   function buildHTML(message){
     if (message.image) {
       var html =
@@ -67,69 +66,15 @@ $(function(){
 
 
   // ここから自動更新機能
-  var buildHTML = function(message) {
-    if (message.content && message.image) {
-      //data-idが反映されるようにしている
-      var html = `<div class="message" data-message-id=` + message.id + `>` +
-        `<div class="user-info">` +
-          `<div class="user-name">` +
-            message.user_name +
-          `</div>` +
-          `<div class="date">` +
-            message.created_at +
-          `</div>` +
-        `</div>` +
-        `<div class="message-text">` +
-          `<p class="lower-message__content">` +
-            message.content +
-          `</p>` +
-          `<img src="` + message.image + `" class="lower-message__image" >` +
-        `</div>` +
-      `</div>`
-    } else if (message.content) {
-      //同様に、data-idが反映されるようにしている
-      var html = `<div class="message" data-message-id=` + message.id + `>` +
-        `<div class="user-info">` +
-          `<div class="user-name">` +
-            message.user_name +
-          `</div>` +
-          `<div class="date">` +
-            message.created_at +
-          `</div>` +
-        `</div>` +
-        `<div class="message-text">` +
-          `<p class="lower-message__content">` +
-            message.content +
-          `</p>` +
-        `</div>` +
-      `</div>`
-    } else if (message.image) {
-      //同様に、data-idが反映されるようにしている
-      var html = `<div class="message" data-message-id=` + message.id + `>` +
-        `<div class="user-info">` +
-          `<div class="user-name">` +
-            message.user_name +
-          `</div>` +
-          `<div class="date">` +
-            message.created_at +
-          `</div>` +
-        `</div>` +
-        `<div class="lower-message__content">` +
-          `<img src="` + message.image + `" class="lower-message__image" >` +
-        `</div>` +
-      `</div>`
-    };
-    return html;
-  };
-
   var reloadMessages = function() {
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    last_message_id = $('.message:last').data("message-id");
+    last_message_id = $('.message:last').data('message-id');
+    console.log(last_message_id);
     $.ajax({
       //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
       url: "api/messages",
       //ルーティングで設定した通りhttpメソッドをgetに指定
-      type: 'get',
+      type: 'GET',
       dataType: 'json',
       //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
@@ -142,17 +87,14 @@ $(function(){
         });
         // メッセージが入ったHTMLに、入れ物ごと追加
         $('.messages').append(insertHTML);
-        $(".chat-main__message-list").animete({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
-        $("#new_message")[0].reset();
-        $(".form__submit").prop("disabled", false);
+        $(".chat-main__message-list").animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
       }
     })
     .fail(function() {
-      console.log('error');
+      console.log('失敗');
     });
-    setInterval(reloadMessages, 7000);
-    if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-      setInterval(reloadMessages, 7000);
-    }
   };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
